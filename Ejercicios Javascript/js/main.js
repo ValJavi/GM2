f08a0277e3b6cb31e40078a9c0f0b3d1031974ca
Javier Valdez <jv.javierval@gmail.com>
2022-05-15 10:37:13 -0300

    Agrega ejercicios 1 y 2 de JS

--- Ejercicios Javascript/js/main.js
+++ Ejercicios Javascript/js/main.js
@@ -1,3 +1,89 @@
+/* Ejercicio 1 */
+
+function sumarAlArray(array, numero){
+    let arrayToNumber = "";
+    for (num of array) {
+        arrayToNumber += num.toString();
+    }
+
+    arrayToNumber = (Number(arrayToNumber) + numero).toString().split("").map(elem => Number(elem) );
+    
+    return arrayToNumber;    
+}
+
+
+function restarAlArray(array, numero){
+    let arrayToNumber = "";
+    for (num of array) {
+        arrayToNumber += num.toString();
+    }
+    
+    (Number(arrayToNumber) - numero) < 0 ? arrayToNumber = [0] : 
+    arrayToNumber = (Number(arrayToNumber) - numero).toString().split("").map(elem => Number(elem));
+
+    return arrayToNumber;    
+}
+
+
+let array1 = [1,2,3];
+let array2 = [1,9];
+let array3 = [];
+let array4 = [2,0];
+let array5 = [3];
+
+
+//console.log(sumarAlArray(array1, 3));
+//console.log(sumarAlArray(array2, 2));
+//console.log(sumarAlArray(array3, 4));
+
+//console.log(restarAlArray(array1, 4));
+//console.log(restarAlArray(array4, 2));
+//console.log(restarAlArray(array5, 1067));
+
+
+
+/* Ejercicio 2 */
+
+
+function hayParOPareja(array) { 
+    let pares = [];
+    let pareja = false;
+    for (num of array) {
+        if (num % 2 === 0 && num !== 0) {
+            pares.push(num);
+        }
+    }
+
+    if (pares.length === 0) return false;
+
+    for (let i = 0; i < array.length; i++){
+        for (let j = i+1; j < array.length; j++) {
+            for (let k = 0; k < pares.length; k++) {
+
+                if (array[i] + array[j] === pares[k]) {
+                    pareja = true; 
+                }
+            }
+        }
+    }
+
+    if (pareja === true) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+//let array = [1, 3, 4, 6, 3, 5]
+//console.log(hayParOPareja(array))
+
+
+/* Ejercicio 3 */
+
+
 function esPalindromo(string){
     let resultado;
 
@@ -11,7 +97,7 @@ function esPalindromo(string){
 
 //console.log(esPalindromo(prompt("Ingresa un texto")));
 
-function esPalindromo2(string) {
+function puedeSerPalindromo(string) {
     let stringArray = string.split("");
     
     for (let i = 0; i < stringArray.length; i++){
@@ -29,4 +115,6 @@ function esPalindromo2(string) {
     return stringArray.length > 1 ? false : true;    
 }
 
-console.log(esPalindromo2(prompt("Ingresa un texto")))
+//console.log(puedeSerPalindromo(prompt("Ingresa un texto")))
+
+
